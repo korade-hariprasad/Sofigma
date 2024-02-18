a2b0fcd2b5536951c195fcd6f9419741a57c6800
hariprasad-korade <hariprasadkorade@gmail.com>
2024-02-18 19:38:09 +0530

    Initial upload

---
+++ Sofigma/Sofigma.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Sofigma
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.rotor1 = New System.Windows.Forms.TextBox()
+        Me.rotor2 = New System.Windows.Forms.TextBox()
+        Me.rotor3 = New System.Windows.Forms.TextBox()
+        Me.input = New System.Windows.Forms.TextBox()
+        Me.output = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.copycode = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.DarkOliveGreen
+        Me.btnReset.FlatAppearance.BorderColor = System.Drawing.Color.DarkOliveGreen
+        Me.btnReset.FlatAppearance.BorderSize = 0
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnReset.Location = New System.Drawing.Point(365, 8)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 31)
+        Me.btnReset.TabIndex = 2
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'rotor1
+        '
+        Me.rotor1.Location = New System.Drawing.Point(78, 10)
+        Me.rotor1.MaxLength = 2
+        Me.rotor1.Name = "rotor1"
+        Me.rotor1.Size = New System.Drawing.Size(28, 20)
+        Me.rotor1.TabIndex = 3
+        '
+        'rotor2
+        '
+        Me.rotor2.Location = New System.Drawing.Point(112, 10)
+        Me.rotor2.MaxLength = 2
+        Me.rotor2.Name = "rotor2"
+        Me.rotor2.Size = New System.Drawing.Size(28, 20)
+        Me.rotor2.TabIndex = 4
+        '
+        'rotor3
+        '
+        Me.rotor3.Location = New System.Drawing.Point(146, 10)
+        Me.rotor3.MaxLength = 2
+        Me.rotor3.Name = "rotor3"
+        Me.rotor3.Size = New System.Drawing.Size(28, 20)
+        Me.rotor3.TabIndex = 5
+        '
+        'input
+        '
+        Me.input.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.input.Location = New System.Drawing.Point(9, 59)
+        Me.input.Name = "input"
+        Me.input.Size = New System.Drawing.Size(431, 20)
+        Me.input.TabIndex = 6
+        '
+        'output
+        '
+        Me.output.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.output.Location = New System.Drawing.Point(9, 146)
+        Me.output.Name = "output"
+        Me.output.ReadOnly = True
+        Me.output.Size = New System.Drawing.Size(431, 20)
+        Me.output.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Ivory
+        Me.Label1.Location = New System.Drawing.Point(6, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 15)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Input"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Ivory
+        Me.Label2.Location = New System.Drawing.Point(6, 130)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 15)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Output"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(6, 11)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 15)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Set Key"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Ivory
+        Me.Label4.Location = New System.Drawing.Point(6, 91)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(142, 15)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Copy input code here"
+        '
+        'copycode
+        '
+        Me.copycode.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.copycode.Location = New System.Drawing.Point(9, 107)
+        Me.copycode.Name = "copycode"
+        Me.copycode.Size = New System.Drawing.Size(273, 20)
+        Me.copycode.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkOliveGreen
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkOliveGreen
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button1.Location = New System.Drawing.Point(288, 98)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(152, 36)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Decode input code"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Sofigma
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkOliveGreen
+        Me.ClientSize = New System.Drawing.Size(452, 177)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.copycode)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.output)
+        Me.Controls.Add(Me.input)
+        Me.Controls.Add(Me.rotor3)
+        Me.Controls.Add(Me.rotor2)
+        Me.Controls.Add(Me.rotor1)
+        Me.Controls.Add(Me.btnReset)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Sofigma"
+        Me.Text = "Sofigma"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnReset As Button
+    Friend WithEvents rotor1 As TextBox
+    Friend WithEvents rotor2 As TextBox
+    Friend WithEvents rotor3 As TextBox
+    Friend WithEvents input As TextBox
+    Friend WithEvents output As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents copycode As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Sofigma/Sofigma.vb
@@ -0,0 +1,251 @@
+﻿Public Class Sofigma
+    Dim ar As Char() = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", " ", "]", "^", "_", "'", "{", "}", "~"}
+    Dim r1 As Char() = {"|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|"}
+    Dim r2 As Char() = {"|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|"}
+    Dim r3 As Char() = {"|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|"}
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        rotor1.Text = 1
+        rotor2.Text = 1
+        rotor3.Text = 1
+        input.Clear()
+        copycode.Clear()
+        output.Clear()
+    End Sub
+
+    Private Sub input_TextChanged(sender As Object, e As EventArgs) Handles input.TextChanged
+        If rotor1.Text = "" Or rotor2.Text = "" Or rotor3.Text = "" Then
+            input.Clear()
+            MsgBox("Please enter valid number")
+        Else
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''setting r1 START''''''''''''''''''''''''''''''''''''''''
+            If rotor1.Text <> "" Then
+                For i = 0 To 63
+                    r1(i) = "|"
+                Next
+                Dim n As Integer = rotor1.Text
+                For i = 0 To 63
+                    If r1(i) = "|" Then
+                        If (i + n) > 63 Then
+                            Dim st As Integer = i
+                            Dim en As Integer = 63
+                            While r1(en) <> "|"
+                                en -= 1
+                            End While
+                            'MsgBox("end = " + en.ToString)
+                            For j = st To en
+                                r1(j) = ar(en)
+                                'MsgBox("r1(" + j.ToString + ") = " + r1(j))
+                                en -= 1
+                            Next
+                        Else
+                            r1(i) = ar(i + n)
+                            'MsgBox("r1(" + i.ToString + ") = " + r1(i))
+                            r1(i + n) = ar(i)
+                            'MsgBox("r1(" + (i + n).ToString + ") = " + r1(i + n))
+                        End If
+                    Else
+                        Continue For
+                    End If
+                Next
+            End If
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''setting r1 END''''''''''''''''''''''''''''''''''''''''
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''SETTING r2 START''''''''''''''''''''''''''''''''''''''''''''''''''
+            If rotor2.Text <> "" Then
+                For i = 0 To 63
+                    r2(i) = "|"
+                Next
+                Dim n As Integer = rotor2.Text
+                For i = 0 To 63
+                    If r2(i) = "|" Then
+                        If (i + n) > 63 Then
+                            Dim st As Integer = i
+                            Dim en As Integer = 63
+                            While r2(en) <> "|"
+                                en -= 1
+                            End While
+                            'MsgBox("end = " + en.ToString)
+                            For j = st To en
+                                r2(j) = ar(en)
+                                'MsgBox("r1(" + j.ToString + ") = " + r1(j))
+                                en -= 1
+                            Next
+                        Else
+                            r2(i) = ar(i + n)
+                            'MsgBox("r1(" + i.ToString + ") = " + r1(i))
+                            r2(i + n) = ar(i)
+                            'MsgBox("r1(" + (i + n).ToString + ") = " + r1(i + n))
+                        End If
+                    Else
+                        Continue For
+                    End If
+                Next
+            End If
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''SETTING r2 END''''''''''''''''''''''''''''''''''''''''''''''''''
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''SETTING r3 START''''''''''''''''''''''''''''''''''''''''''''''''''
+            If rotor3.Text <> "" Then
+                For i = 0 To 63
+                    r2(i) = "|"
+                Next
+                Dim n As Integer = rotor3.Text
+                For i = 0 To 63
+                    If r3(i) = "|" Then
+                        If (i + n) > 63 Then
+                            Dim st As Integer = i
+                            Dim en As Integer = 63
+                            While r3(en) <> "|"
+                                en -= 1
+                            End While
+                            'MsgBox("end = " + en.ToString)
+                            For j = st To en
+                                r3(j) = ar(en)
+                                'MsgBox("r1(" + j.ToString + ") = " + r1(j))
+                                en -= 1
+                            Next
+                        Else
+                            r3(i) = ar(i + n)
+                            'MsgBox("r1(" + i.ToString + ") = " + r1(i))
+                            r3(i + n) = ar(i)
+                            'MsgBox("r1(" + (i + n).ToString + ") = " + r1(i + n))
+                        End If
+                    Else
+                        Continue For
+                    End If
+                Next
+            End If
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''SETTING r3 END''''''''''''''''''''''''''''''''''''''''''''''''''
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''ENCODING START'''''''''''''''''''''''''''''''''''''''''''''''
+            If input.Text <> "" Then
+                Dim ip As Integer
+                Dim op As Char
+                Dim ip_str As String = input.Text
+                Dim op_str As String = output.Text
+                For i = 0 To 63
+                    If ar(i) = ip_str(ip_str.Length - 1) Then
+                        ip = i
+                        op = r1(ip)
+                        op_str = op_str + op
+                    End If
+                Next
+                'MsgBox(op)
+                'For i = 0 To 63
+                '    If ar(i) = op Then
+                '        ip = i
+                '        op = r2(ip)
+                '        op_str = op_str + op
+                '    End If
+                'Next
+                'MsgBox(op)
+                'For i = 0 To 63
+                '    If ar(i) = op Then
+                '        ip = i
+                '        op = r3(ip)
+                '        op_str = op_str + op
+                '    End If
+                'Next
+                'MsgBox(op)
+                output.Text = op_str
+                rotor1.Text = rotor1.Text + 1
+                ''''''''''''''''''''''''''''''''''''''CHECK ROTOR POSITIONS START''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+                If rotor1.Text > 63 Then
+                    rotor2.Text = rotor2.Text + 1
+                    For i = 0 To 63
+                        r1(i) = "|"
+                    Next
+                    If rotor2.Text > 63 Then
+                        rotor3.Text = rotor3.Text + 1
+                        For i = 0 To 63
+                            r2(i) = "|"
+                        Next
+                        If rotor3.Text > 63 Then
+                            rotor3.Text = 1
+                            rotor2.Text = 1
+                            rotor1.Text = 1
+                            For i = 0 To 63
+                                r1(i) = "|"
+                                r2(i) = "|"
+                                r3(i) = "|"
+                            Next
+                        End If
+                        rotor2.Text = 1
+                    End If
+                    rotor1.Text = 1
+                End If
+                ''''''''''''''''''''''''''''''''''''''CHECK ROTOR POSITIONS END''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+            End If
+            Dim a As Integer = 34'''''''''''''''''''double quote
+            Dim b As Integer = 124''''''''''''''''''logical or
+            Dim c As Integer = 47'''''''''''''''''''slash
+            If input.Text.Contains("`") Or input.Text.Contains(Convert.ToChar(a)) Or input.Text.Contains(Convert.ToChar(b)) Or input.Text.Contains(Convert.ToChar(c)) Then
+                MsgBox("Please do not enter double quotation marks instead use single quotation marks. Please do not enter ` or |(this is pipe not capital i) or slash. Now reset the machine, set key again & start over. Remember you will not get correct value if u use backspace in input you will need to reset the machine")
+            End If
+            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''ENCODING END'''''''''''''''''''''''''''''''''''''''''''''''
+        End If
+    End Sub
+
+    Private Sub output_TextChanged(sender As Object, e As EventArgs) Handles output.TextChanged
+        Dim o As String = output.Text
+        o = o.Trim("|")
+        output.Text = o
+    End Sub
+
+    Private Sub rotor1_TextChanged(sender As Object, e As EventArgs) Handles rotor1.TextChanged
+        Dim num As Integer
+        If Integer.TryParse(rotor1.Text, num) Then
+            If rotor1.Text = 0 Then
+                rotor1.Text = 1
+                MsgBox("Please enter valid number")
+            End If
+        Else
+            rotor1.Text = 1
+            MsgBox("Please enter valid number")
+        End If
+    End Sub
+
+    Private Sub rotor2_TextChanged(sender As Object, e As EventArgs) Handles rotor2.TextChanged
+        Dim num As Integer
+        If Integer.TryParse(rotor2.Text, num) Then
+            If rotor2.Text = 0 Then
+                rotor2.Text = 1
+                MsgBox("Please enter valid number")
+            End If
+        Else
+            rotor2.Text = 1
+            MsgBox("Please enter valid number")
+        End If
+    End Sub
+
+    Private Sub rotor3_TextChanged(sender As Object, e As EventArgs) Handles rotor3.TextChanged
+        Dim num As Integer
+        If Integer.TryParse(rotor3.Text, num) Then
+            If rotor3.Text = 0 Then
+                rotor3.Text = 1
+                MsgBox("Please enter valid number")
+            End If
+        Else
+            rotor3.Text = 1
+            MsgBox("Please enter valid number")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        input.Clear()
+        output.Clear
+        Dim copy As Char() = copycode.Text
+        For i = 0 To copycode.Text.Length - 1
+            input.Text = input.Text + copy(i)
+        Next
+    End Sub
+
+    Private Sub copycode_TextChanged(sender As Object, e As EventArgs) Handles copycode.TextChanged
+        input.Clear()
+        output.Clear()
+        rotor1.Text = 1
+        rotor2.Text = 1
+        rotor3.Text = 1
+    End Sub
+
+    Private Sub Sofigma_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Remember you will need to RESET THE MACHINE & START ALL OVER AGAIN if you did a mistake in input. You CANNOT rely on backspace as it leads to wrong encoding-decoding. So better be careful while typing the input.")
+    End Sub
+End Class
